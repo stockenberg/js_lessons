c678a2ad7e43ab99ff0cd96e232a70015e7a5029
Marten Stockenberg <MStockenberg@googlemail.com>
2017-02-06 15:11:02 +0100

    Traversing and HTML Edit

---
+++ js/html-manipulation.js
@@ -0,0 +1,35 @@
+/**
+ * Created by workstation on 06.02.17.
+ */
+
+
+/**
+ // GET / SET
+ .text();
+ .html();
+ .val();
+ .attr();
+
+ // Elements
+ .append();
+ .prepend();
+ .after();
+ .before();
+ .remove();
+ .empty();
+ .addClass();
+ .removeClass();
+ .toggleClass();
+ .css(
+    {"property":"value", "prop": "val"}
+ );
+
+ // Dimensions
+ .width();
+ .height();
+ .innerWidth();
+ .innerHeight();
+ .outerWidth();
+ .outerHeight();
+ */
+
---
+++ js/traversing.js
@@ -0,0 +1,23 @@
+/**
+ * Created by workstation on 06.02.17.
+ */
+
+/**
+ * .parent();
+ * .parents("filter");
+ * $("start").parentsUntil("stop");
+ * .children("filter");
+ * .find("filter");
+ * .siblings()
+ * .next()
+ * .nextAll()
+ * .nextUntil()
+ * .prev()
+ * .prevAll()
+ * .prevUntil();
+ * .first();
+ * .last();
+ * .eq();
+ * .filter();
+ * .not();
+ */
